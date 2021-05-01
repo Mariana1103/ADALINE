1c0833c74e6d8cd61059a88d6a5f7a556946184a
Nefonfo <victorarmenta30@gmail.com>
2021-05-01 14:52:08 -0500

    Version final

--- adaline.c
+++ adaline.c
@@ -5,26 +5,46 @@
 #define ROWS 7
 #define COLS 3
 
+// D ES EL VALOR QUE SE DESEA
+// Y ES EL VALOR CALCULADO
+// 1
+// 0.783 
 float predict(int x[], float w[])
 {
 	// Y = SUMATORIA DE Xi + Wi
+	// [X1, X2, X3] -> [W1, W2, W3]
+	// [0, 0, 1] -> [0.85, 2.0, 0.2]
+	// Y = (X1*W1) + (X2*W2) + (X3*W3) .... + (Xn * Wn)
 	// X = VALORES DE LOS BINARIOS (X1, X2, X3)
 	// W = VALORES DE LOS PESOS (W1, W2, W3)
 	float result = 0;
 	for (int i = 0; i <= COLS - 1; i++)
 	{
-		printf("result-> %f += %d * %f\n", result, x[i], w[i]);
+        // PRIMERA ITERACION
+		// Y = (X1*W1)
+		// SEGUNDA ITERACION
+		// Y = (X1*W1) + (X2*W2)
+		// TERCERA ITERACION
+		// Y = (X1*W1) + (X2*W2) + (X3*W3)
 		result += x[i] * w[i];
 	}
+    // FLOAT Y = (X1*W1) + (X2*W2) + (X3*W3) .... + (Xn * Wn)
 	return result;
 }
 
+
+// X_row = [0, 0, 1]
+// INCREMENTO EN iWj = delta * ERROR * Xi
+// w = [1, 0.5, 3]
+// W = W + iWj
+// APUNTADOR = DIRECCION DE MEMORIA
+// float variable;
+// void scanf("%f", &variable);
 void fit(float delta, float error, int x_row[], float (*w)[])
 {
 	printf("VALOR -> %d %d %d\n", x_row[0], x_row[1], x_row[2]);
 	for (int i = 0; i <= COLS - 1; i++)
 	{
-		printf("w = %f + (%f*%f*%d)\n", (*w)[i], delta, error, x_row[i]);
 		(*w)[i] = (*w)[i] + (delta * error * x_row[i]);
 	}
 }
@@ -32,7 +52,7 @@ void fit(float delta, float error, int x_row[], float (*w)[])
 void read_binary(int (*direction)[3]){
 	for(int x = 0; x < 3; x++){
 		int value;
-		printf("INGRESE EL BINARIO: %d\n", x);
+		printf("INGRESE EL DIGITO [%d]:", x);
 		scanf("%d", &value);
 		if(value != 0 && value != 1) {
 			printf("VALOR INGRESADO ES INCORRECTO %d\n", value);
@@ -48,7 +68,7 @@ void read_result(int index, int *direction){
 	int value;
 	bool save = false;
 	while(!save){
-		printf("ESCRIBA EL RESULTADO:\n");
+		printf("ESCRIBA EL VALOR ESPERADO:\n");
 		scanf("%d", &value);
 		if(value >= 0 && value <= 7){
 			(*direction) = value;
@@ -66,26 +86,33 @@ int main()
 	float error_level;
 	float w[COLS] = {3.12, 2.00, 1.86};
 	bool error_passed = false;
-	char exit = 'Y';
+	char exit='Y';
 
-	int X_Train[ROWS][COLS];
-	int y_Train[ROWS];
+	int X_Train[ROWS][COLS] = {
+		{0,0,1},
+		{0,1,0},
+		{0,1,1},
+		{1,0,0},
+		{1,0,1},
+		{1,1,0},
+		{1,1,1},
+	};
+	int y_Train[ROWS] = {1,2,3,4,5,6,7};
 
 	int X_Test[COLS];
 
-	printf("ESCOJA EL NIVEL DE DELTA:\n");
+	printf("INGRESE EL VALOR DE DELTA:\n");
 	scanf("%f", &delta);
-	printf("ESCOJA EL NIVEL DE ERROR:\n");
+	printf("INGRESE EL ERROR DESEADO:\n");
 	scanf("%f", &error_level);
 
 
-	
 	while(!error_passed) {
 		printf("----------- ITERACIÓN %d ------------\n", fors);
-		for(int i = 0; i <= ROWS-1; i++){
+		/*for(int i = 0; i <= ROWS-1; i++){
 			read_binary(&X_Train[i]);
 			read_result(i, &y_Train[i]);
-		}
+		}*/
 		for(int i = 0; i <= ROWS-1; i++) {
 			printf("%d %d %d -> %d\n", X_Train[i][0], X_Train[i][1], X_Train[i][2], y_Train[i]);
 		}
@@ -109,9 +136,7 @@ int main()
 			printf("W2: %f\n", w[1]);
 			printf("W3: %f\n", w[2]);
 			printf("+++++++++++++++++++++++++++++++++++++\n");
-			
-			
-			
+
 		}
 
 		printf("--------------------------------\n");
@@ -119,13 +144,14 @@ int main()
 		fors++;
 	}
 
-
-	printf("------------- EJERCICIOS ------------\n");
-	while(exit == 'Y' || exit == 'y')
-	{
-		read_binary(&X_Test);
-		printf("EL RESULTADO ES DE: %f\n", predict(X_Test, w));
-		printf("DESEA CONTINUAR? PRESIONE Y/y \n");
-		scanf(" %c", &exit);
+	if(error_passed==true){
+		printf("------------- EJERCICIOS ------------\n");
+		while(exit == 'y' || exit=='Y')
+		{
+			read_binary(&X_Test);
+			printf("EL RESULTADO ES DE: %f\n", predict(X_Test, w));
+			printf("DESEA CONTINUAR? PRESIONE Y/y \n");
+			scanf(" %c", &exit);
+		}
 	}
 }
